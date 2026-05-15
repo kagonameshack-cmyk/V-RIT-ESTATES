7696070404ea72ab14c06ff0b8c5f6ef10d8597a
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-16 00:27:36 +0300

    script.js

--- script.js
+++ script.js
@@ -57,3 +57,51 @@ mobileLinks.forEach(link => {
     });
 
 });
+
+
+const sections =
+document.querySelectorAll(
+"section, footer"
+);
+
+const navLinks =
+document.querySelectorAll(
+"nav a, .mobile-menu a"
+);
+
+window.addEventListener(
+"scroll",
+() => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop =
+        section.offsetTop;
+
+        if(pageYOffset >= sectionTop - 200){
+
+            current =
+            section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if(
+        link.getAttribute("href")
+        === `#${current}`
+        ){
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
