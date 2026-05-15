8d676c627bca66b7ce8e4e8fc1bbd0e4bdeb486e
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-16 00:50:43 +0300

    script.js

--- script.js
+++ script.js
@@ -105,3 +105,23 @@ window.addEventListener(
     });
 
 });
+
+
+const header =
+document.querySelector("header");
+
+window.addEventListener(
+"scroll",
+() => {
+
+    if(window.scrollY > 50){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
